e5d171eadb9ddfa061e5b81bbba9f04ae772d77e
Your Name <you@example.com>
2018-01-06 17:30:51 +0000

    feature: Now displays the total value of the coinspossessed.

--- lib/mycoins.rb
+++ lib/mycoins.rb
@@ -3,14 +3,17 @@
 # file: mycoins.rb
 
 require 'dynarex'
+require 'justexchangerates'
 require 'cryptocoin_fanboi'
 
 
 class MyCoins
 
-	def initialize(source, date: nil, debug: false)
+  def initialize(source, date: nil, debug: false, mycurrency: 'USD')
 
-    @debug = debug
+    @debug = debug    
+    
+    @jer = JustExchangeRates.new(base: 'USD')
 
     s = RXFHelper.read(source).first
 
@@ -24,6 +27,9 @@ class MyCoins
     c = CryptocoinFanboi.new
 
     puts '@dx.to_xml: ' + @dx.to_xml if @debug
+
+    @mycurrency = @dx.currency || mycurrency.upcase
+    puts '@mycurrency: ' + @mycurrency.inspect if @debug
 
     mycoins = @dx.all.inject([]) do |r, mycoin|
       
@@ -40,4 +46,56 @@ class MyCoins
   def to_s()
     @ccf.to_s
   end
+  
+  def portfolio()
+        
+    # print out the coin name, qty,value in USD,value in Sterling, and BTC
+    a = @dx.all.inject([]) do |r, x|
+
+      puts 'x: ' + x.inspect if @debug
+      usd_rate = @ccf.find(x.title).price_usd.to_f      
+      
+      h = {
+        name: x.title, 
+        qty: x.qty, 
+        value_usd: (usd_rate * x.qty.to_f).round(2)
+      }
+      
+
+      mycurrency = if @mycurrency and @mycurrency != 'USD' then
+        
+        local_value = ((usd_rate * x.qty.to_f) * @jer.rate(@mycurrency)).round(2)  
+        h.merge!(('value_' + @mycurrency.downcase).to_sym => local_value)
+
+        
+        @mycurrency
+        
+      else
+        
+        'USD'
+        
+      end
+      
+      r << h
+      
+    end
+
+    coins = a.map {|x| x.values}
+    
+    labels = %w(Name Qty USD)    
+    labels << @mycurrency if @mycurrency    
+    
+    puts 'labels: ' + labels.inspect if @debug
+    tf = TableFormatter.new(source: coins, labels: labels)
+    out = tf.display
+    
+    total = a.inject(0) do |r, x|
+      r + x[('value_' + @mycurrency.downcase).to_sym]
+    end
+    
+    out << "\n\nTotal: %s %s" % [total.round(2), @mycurrency]
+    out
+
+  end
+  
 end
--- mycoins.gemspec
+++ mycoins.gemspec
@@ -1,11 +1,13 @@
 Gem::Specification.new do |s|
   s.name = 'mycoins'
-  s.version = '0.1.0'
-  s.summary = 'mycoins'
+  s.version = '0.2.0'
+  s.summary = 'The mycoins gem is intended to show the current ' + 
+      'value of your cryptocoins.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/mycoins.rb']
   s.add_runtime_dependency('dynarex', '~> 1.7', '>=1.7.27')
-  s.add_runtime_dependency('cryptocoin_fanboi', '~> 0.2', '>=0.2.3')
+  s.add_runtime_dependency('justexchangerates', '~> 0.2', '>=0.2.0')
+  s.add_runtime_dependency('cryptocoin_fanboi', '~> 0.2', '>=0.2.4')
   s.signing_key = '../privatekeys/mycoins.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
